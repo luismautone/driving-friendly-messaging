52569724a97349b67fcc105a950793264fa41e99
Luis Mautone <lorenzo.pongetti@gmail.com>
2020-03-29 21:12:55 +0200

    Initial Commit

---
+++ Driver-Friendly Messaging App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Driver-Friendly Messaging App
+//
+//  Created by Luis Mautone on 29/03/2020.
+//  Copyright © 2020 Luis Mautone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
